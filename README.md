641486bd58e82671314332ad14b2014d9f7be640
libc0607 <libc0607@gmail.com>
2017-04-22 19:09:58 +0800

    加入一个写起来非常麻烦的白学剧本功能然后有空会重新写的

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Haruki_bot
-因为总是玩坏别人的Bot所以还是自己写一个吧Orz  
-对不起各位被我玩坏过Bot的dalao们
+# Haruki_bot  
+因为总是玩坏别人的Bot所以还是自己写一个吧Orz    
+对不起各位被我玩坏过Bot的dalao们   
+主要参考了@fython dalao的代码（第一次玩这个
--- main.py
+++ main.py
@@ -3,34 +3,53 @@
 
 import os
 import random
+import time
 import telebot
-from bot_token import BOT_TOKEN
+from bot_token import HARUKI_BOT_TOKEN
+from bot_token import KAZUSA_BOT_TOKEN
 from x3xb import X3XB
+from sinario import SINARIO
 #import bot_utils
 
 
-haruki = telebot.TeleBot(BOT_TOKEN)
-me = haruki.get_me()
+haruki = telebot.TeleBot(HARUKI_BOT_TOKEN)
+kazusa = telebot.TeleBot(KAZUSA_BOT_TOKEN)
+print('Bot Account: ', haruki.get_me())
+print('Bot Account: ', kazusa.get_me())
 
-print('Bot Account: ', me)
 
 @haruki.message_handler(commands=['start'])
 def send_welcome(message):
     haruki.reply_to(message, 'start')
 
+@haruki.message_handler(commands=['help'])
+def send_help(message):
+    haruki.reply_to(message, '源码在https://github.com/libc0607/Haruki_bot，抄了很多@fython的Bot代码')
+
+@haruki.message_handler(commands=['replay'])
+def send_replay(message):
+    print('white!')
+    white_script = random.choice(SINARIO)
+    for sentence in white_script:
+        print(sentence)
+        time.sleep(sentence[1])
+        if (sentence[0] == 'haruki'):
+            haruki.send_message(message.chat.id, sentence[2])
+        elif (sentence[0] == 'kazusa'):
+            kazusa.send_message(message.chat.id, sentence[2])
+        # 雪菜嘛 以后会有的
+
+
 @haruki.message_handler(func=lambda message: True)
 def x3xb(message):
-    for index in range(len(X3XB)):
-        if (X3XB[index] in message.text):
-            haruki.send_message(message.chat.id, X3XB[-index-1])
-
-
-
-
-
+    for key in X3XB:
+        if (key in message.text):
+            haruki.send_message(message.chat.id, X3XB[key])
+            kazusa.send_message(message.chat.id, X3XB[key])
 
 
 
 
 
 haruki.polling()
+kazusa.polling()
